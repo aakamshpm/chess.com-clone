3579d1d2579517866497e825f0c38208c8f7cf31
Aakamsh PM <aakamshpm@gmail.com>
2025-06-23 02:14:04 +0530

    backend initial setup wrote

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,24 @@
 import express from "express";
-import socket from "socket.io";
 import http from "http";
+import ejs from "ejs";
+import { Server } from "socket.io";
 import { Chess } from "chess.js";
+import path from "path";
 
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+const chess = new Chess();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
