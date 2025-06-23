95408b08f38c5ae0d72a52ed624c246b3e257e1f
Aakamsh PM <aakamshpm@gmail.com>
2025-06-23 22:04:42 +0530

    configuerd server with express and client with vite(html)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,42 @@
 import express from "express";
 import http from "http";
-import ejs from "ejs";
 import { Server } from "socket.io";
 import { Chess } from "chess.js";
 import path from "path";
+import cors from "cors";
 
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+const PORT = process.env.PORT || 3000;
+const CLIENT_PORT = 5173;
+
+app.use(cors());
+app.use(express.json());
 
 const chess = new Chess();
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/", (req, res) => {
-  res.render("index");
+// Basic route
+app.get("/api/health", (req, res) => {
+  res.json({ status: "OK", message: "Server is running!" });
 });
 
-server.listen(3000, () => {
+io.on("connection", (socket) => {
+  console.log("A user connected: " + socket.id);
+});
+
+server.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
+  console.log(
+    `Please manually open http://localhost:${CLIENT_PORT} to access the client.`
+  );
 });
